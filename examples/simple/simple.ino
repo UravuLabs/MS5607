96219a6fb4466a6eee1ad9c1f8cf4cd753ba6b90
UravuLabs <mail@uravulabs.com>
2018-09-20 17:19:39 +0530

    typo fixed

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,4 +1,4 @@
-#include<../MS5607.h>
+#include<MS5607.h>
 
 MS5607 P_Sens;
 
@@ -7,12 +7,11 @@ void setup(void){
   if(!P_Sens.begin()){
     Serial.println("Error in Communicating with sensor, check your connections!");
   }else{
-    Serial.prinln("MS5607 initialization successful!");
+    Serial.println("MS5607 initialization successful!");
   }
 }
-
+float P_val,T_val,H_val;
 void loop(void){
-  float P_val,T_val,H_val;
   if(P_Sens.readDigitalValue()){
     T_val = P_Sens.getTemperature();
     P_val = P_Sens.getPressure();
@@ -21,12 +20,15 @@ void loop(void){
     Serial.println("Error in reading digital value in sensor!");
   }
 
-  Serial.print("Temperature : ");
-  Serial.println(T_val);
-  Serial.print("Pressure    : ");
-  Serial.println(P_val);
-  Serial.print("Altitude    : ");
-  Serial.println(H_val);
-  
+  Serial.print("Temperature :  ");
+  Serial.print(T_val);
+  Serial.println(" C");
+  Serial.print("Pressure    :  ");
+  Serial.print(P_val);
+  Serial.println(" mBar");
+  Serial.print("Altitude    :  ");
+  Serial.print(H_val);
+  Serial.println(" meter");
+
   delay(1000);
 }
